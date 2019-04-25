6ed65a5e461d218769578404c3bbed16c87e6b52
Anirudh Kulkarni <k.anirudh8@gmail.com>
2019-04-24 23:26:38 -0400

    Established connection with mongoDB

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
+import os
 from flask import Flask
 from flask import render_template
+from pymongo import MongoClient
+import urllib
+import json
+import pandas as pd
+
 
 app = Flask(__name__)
+
+
+
+# Info for local connection to MongoDB
+DBS_NAME = os.getenv('DBS_NAME', 'language_popularity')
+COLLECTION_NAME = 'stackoverflow'
 
 # Routes
 @app.route("/")
@@ -11,6 +23,48 @@ def index():
     """
     return render_template("index.html")
 
+
+@app.route("/init")
+def init_data():
+    df = pd.read_csv("survey_results_public.csv")
+
+
+    with MongoClient(MONGODB_URI) as connection:
+        # Define which collection we wish to access
+        collection = connection[DBS_NAME][COLLECTION_NAME]
+        # Retrieve a result set only with the fields defined in FIELDS
+        # projects = collection.find(projection=FIELDS)
+        for rec in df.iloc[5:].to_dict('records'):
+            collection.insert_one(rec)
+
+        # Convert projects to a list in a JSON object and return the JSON data
+        #return json.dumps(list(projects))
+
+@app.route("/data")
+def dummy():
+    """
+    A Flask view to serve projected data from MongoDB in JSON format
+    """
+    # A constant that defines the record fields that we wish to retrieve
+    # Need to explicitly exclude '_id' field as it it automatically included in the retured documents
+    FIELDS = {
+        '_id': False, 'Professional': True, 'Country': True,
+        'EmploymentStatus': True, 'FormalEducation': True, 'MajorUndergrad': True,
+        'YearsProgram': True, 'DeveloperType': True, 'EducationTypes': True,
+        'HaveWorkedLanguage': True, 'HaveWorkedFramework': True, 'HaveWorkedDatabase': True,
+        'TabsSpaces': True
+    }
+
+    # Open a connection to MongoDB using a 'with' statement such that the
+    # connection will be closed as soon as we exit the 'with' statement
+    with MongoClient(MONGODB_URI) as connection:
+        # Define which collection we wish to access
+        collection = connection[DBS_NAME][COLLECTION_NAME]
+        # Retrieve a result set only with the fields defined in FIELDS
+        projects = collection.find(projection=FIELDS, limit=1000)
+
+        # Convert projects to a list in a JSON object and return the JSON data
+        return json.dumps(list(projects))
 
 if __name__ == '__main__':
     app.run(debug=True)
